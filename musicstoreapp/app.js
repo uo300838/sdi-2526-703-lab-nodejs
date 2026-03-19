fd5f3e65ebd6fb1291cc21b922b740bb480e078c
Omayma <uo300838@uniovi.es>
2026-03-19 15:37:44 +0100

    SDI-2526-703-8.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -9,7 +9,6 @@ var usersRouter = require('./routes/users');
 
 
 var app = express();
-require("./routes/songs.js")(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+require("./routes/songs.js")(app);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
