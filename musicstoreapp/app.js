09ac5aeb141808305e90276cb7e18e7645b2d6ca
Omayma <uo300838@uniovi.es>
2026-04-15 22:04:50 +0200

    SDI-2526-703-11.3-SW Cliente JQuery + AJAX.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -31,6 +31,23 @@ favoriteSongsRepository.init(app, dbClient);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
 
+// CORS (desarrollo): permitir peticiones desde cualquier origen (incluyendo header token).
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, token"
+  );
+
+  // Preflight
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
